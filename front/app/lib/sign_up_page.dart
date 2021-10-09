8563cfd9feba2804aef4d187a34f01cdd3b27b43
petrashin <88317323+petrashin@users.noreply.github.com>
2021-10-09 21:29:50 +0300

    new changes

--- front/app/lib/sign_up_page.dart
+++ front/app/lib/sign_up_page.dart
@@ -1,5 +1,5 @@
 // ignore_for_file: use_key_in_widget_constructors, duplicate_ignore, prefer_const_constructors, prefer_const_literals_to_create_immutables
-//
+///
 import 'package:flutter/material.dart';
 import 'quiz_page.dart';
 
