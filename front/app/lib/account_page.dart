e7297e909362c7b864ba3f57bfd4ebe9485e0b95
petrashin <88317323+petrashin@users.noreply.github.com>
2021-10-09 21:24:39 +0300

    new changes

--- front/app/lib/account_page.dart
+++ front/app/lib/account_page.dart
@@ -1,5 +1,5 @@
 // ignore_for_file: use_key_in_widget_constructors, duplicate_ignore, prefer_const_constructors, prefer_const_literals_to_create_immutables, unused_element, avoid_unnecessary_containers, deprecated_member_use, sized_box_for_whitespace
-//
+
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
